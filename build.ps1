9460ab1376e4be8808dc676123bbda3b17c54325
John Edward Willman V <john.willman.1@us.af.mil>
2025-11-29 21:49:53 -0600

    fix build.ps1

--- build.ps1
+++ build.ps1
@@ -6,17 +6,17 @@ param (
 )
 Set-Location $PSScriptRoot
 
-Start-Process uv -ArgumentList "run pyinstaller --onefile --noconsole --distpath=dist/Windows/ --paths=src/Windows --name ClassificationBanner src/Windows/main.py" -wait
+Start-Process uv -ArgumentList "run pyinstaller --onefile --noconsole --distpath="$PSScriptRoot/dist/Windows/" --paths="$PSScriptRoot/src/Windows" --name ClassificationBanner "$PSScriptRoot/src/Windows/main.py"" -wait
 
-Get-ChildItem ".\ClassificationBanner_Install" | ForEach-Object {
+Get-ChildItem "$PSScriptRoot\ClassificationBanner_Install" | ForEach-Object {
     Copy-Item $_.FullName .\dist\Windows -Recurse
 }
-Get-ChildItem ".\src\Windows\Group Policy" | ForEach-Object {
+Get-ChildItem "$PSScriptRoot\src\Windows\Group Policy" | ForEach-Object {
     Copy-Item $_.FullName .\dist\Windows\SupportFiles -Recurse
 }
-Move-Item ".\dist\Windows\ClassificationBanner.exe" ".\dist\Windows\Files\ClassificationBanner.exe"
+Move-Item "$PSScriptRoot\dist\Windows\ClassificationBanner.exe" "$PSScriptRoot\dist\Windows\Files\ClassificationBanner.exe"
 
-(Get-Content ".\dist\Windows\Invoke-AppDeployToolkit.ps1") -replace "AppVersion\s*=.*", "AppVersion                  = `'$($Version.Substring(1))`'" | Set-Content ".\dist\Windows\Invoke-AppDeployToolkit.ps1" -Force
+(Get-Content "$PSScriptRoot\dist\Windows\Invoke-AppDeployToolkit.ps1") -replace "AppVersion\s*=.*", "AppVersion                  = `'$($Version.Substring(1))`'" | Set-Content ".\dist\Windows\Invoke-AppDeployToolkit.ps1" -Force
 
 
-Compress-Archive -Path .\dist\Windows\ -DestinationPath ".\dist\ClassificationBanner-$Version.zip"
+Compress-Archive -Path "$PSScriptRoot\dist\Windows\" -DestinationPath "$PSScriptRoot\dist\ClassificationBanner-$Version.zip"
