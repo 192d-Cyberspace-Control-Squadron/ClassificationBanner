2808583879ca2c7682306c772f71e53110d0d60e
John Edward Willman V <john.willman.1@us.af.mil>
2025-11-29 21:05:02 -0600

    update build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
 Start-Process uv -ArgumentList "run pyinstaller --onefile --noconsole --distpath=dist/Windows/ --paths=src/Windows --name ClassificationBanner src/Windows/main.py"
-Copy-Item ".\src\Windows\Group Policy" .\dist\Windows -Recurse
-Compress-Archive -Path .\dist\Windows\ 
+
+Get-ChildItem ".\ClassificationBanner_Install" | ForEach-Object {
+    Copy-Item $_.FullName .\dist\Windows -Recurse
+}
+Copy-Item ".\src\Windows\Group Policy" .\dist\Windows\SupportFiles -Recurse
+Move-Item ".\dist\Windows\ClassificationBanner.exe" ".\dist\Windows\Files\ClassificationBanner.exe"
+
+Compress-Archive -Path .\dist\Windows\
