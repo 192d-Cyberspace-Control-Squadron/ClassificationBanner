77ac1ac12320dad5b07e76ab883cd2b7a2045546
John Edward Willman V <john.willman.1@us.af.mil>
2025-11-29 21:36:08 -0800

    release v1.3.1
    Fixed workflow and allowed for dynamic updating of version number.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,20 @@
-Start-Process uv -ArgumentList "run pyinstaller --onefile --noconsole --distpath=dist/Windows/ --paths=src/Windows --name ClassificationBanner src/Windows/main.py"
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $Version
+)
+Start-Process uv -ArgumentList "run pyinstaller --onefile --noconsole --distpath=dist/Windows/ --paths=src/Windows --name ClassificationBanner src/Windows/main.py" -wait
 
 Get-ChildItem ".\ClassificationBanner_Install" | ForEach-Object {
     Copy-Item $_.FullName .\dist\Windows -Recurse
 }
-Copy-Item ".\src\Windows\Group Policy" .\dist\Windows\SupportFiles -Recurse
+Get-ChildItem ".\src\Windows\Group Policy" | ForEach-Object {
+    Copy-Item $_.FullName .\dist\Windows\SupportFiles -Recurse
+}
 Move-Item ".\dist\Windows\ClassificationBanner.exe" ".\dist\Windows\Files\ClassificationBanner.exe"
 
-Compress-Archive -Path .\dist\Windows\
+(Get-Content ".\dist\Windows\Invoke-AppDeployToolkit.ps1") -replace "AppVersion\s*=.*", "AppVersion                  = `'$($Version.Substring(1))`'" | Set-Content ".\dist\Windows\Invoke-AppDeployToolkit.ps1" -Force
+
+
+Compress-Archive -Path .\dist\Windows\ -DestinationPath ".\dist\ClassificationBanner-$Version.zip"
